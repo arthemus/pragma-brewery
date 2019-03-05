2051e97325968ec892d5afd97507c91119741588
Arthemus <arthemus.moreira@gmail.com>
2019-03-05 21:03:43 +1100

    Create a new project separation between back and front side.

--- backend/index.js
+++ backend/index.js
@@ -9,10 +9,5 @@ const args = process.argv.slice(2)[0]
 const sourceData = args || './data/mock-beers.json'
 const beersData = JSON.parse(fs.readFileSync(sourceData, 'utf8'))
 const beerObjects = beersData.map((d) => new Beer(d))
-setInterval(() => BeerConsole.init(beerObjects, RandomThermostat), 5000)
 
-// const express = require("express");
-// const PORT = process.env.PORT || 8000;
-// const app = express();
-// app.use(express.static("./frontend/dist/"));
-// app.listen(PORT, () => console.log(`Talent View listening on port ${PORT}!`));
+setInterval(() => BeerConsole.init(beerObjects, RandomThermostat), 5000)

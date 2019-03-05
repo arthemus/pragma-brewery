877b158d4d60306cb6b3b371fbc8bc4e73f2c2fc
Arthemus <arthemus.moreira@gmail.com>
2019-03-06 02:24:03 +1100

    Fix static files.

--- index.js
+++ index.js
@@ -3,13 +3,13 @@
 require('dotenv').config()
 
 const express = require('express')
-const cors = require('cors')
+// const cors = require('cors')
 const app = express()
 
-app.use(cors({
-  origin: process.env.FRONTEND_HOST || 'http://localhost:3000',
-  optionsSuccessStatus: 200
-}))
+// app.use(cors({
+//   origin: process.env.FRONTEND_HOST || 'http://localhost:3000',
+//   optionsSuccessStatus: 200
+// }))
 
 app.use(express.static(__dirname + '/frontend/build'))
 // require('./backend/src/route/main.route')(app)
